5bda50e4809e685b2eaae851a1aadb3c2adb4189
Ben <selahben@hotmail.com>
2023-06-20 20:03:45 +0300

    unique key error fix

--- biz-cards-front/src/exampleCards.js
+++ biz-cards-front/src/exampleCards.js
@@ -1,5 +1,6 @@
 export const exampleCards = [
   {
+    _id: 1,
     bizName: "Example Biz1 Name",
     bizDescription: "Example Biz1 Description",
     bizAddress: "Example Biz1 Address",
@@ -8,6 +9,7 @@ export const exampleCards = [
       "https://cdn.pixabay.com/photo/2015/02/02/11/09/office-620822_1280.jpg",
   },
   {
+    _id: 2,
     bizName: "Example Biz2 Name",
     bizDescription: "Example Biz2 Description",
     bizAddress: "Example Biz2 Address",
@@ -16,6 +18,7 @@ export const exampleCards = [
       "https://cdn.pixabay.com/photo/2015/01/09/11/08/startup-594090_1280.jpg",
   },
   {
+    _id: 3,
     bizName: "Example Biz3 Name",
     bizDescription: "Example Biz3 Description",
     bizAddress: "Example Biz3 Address",
@@ -24,6 +27,7 @@ export const exampleCards = [
       "https://cdn.pixabay.com/photo/2015/01/08/18/27/startup-593341_1280.jpg",
   },
   {
+    _id: 4,
     bizName: "Example Biz4 Name",
     bizDescription: "Example Biz4 Description",
     bizAddress: "Example Biz4 Address",
