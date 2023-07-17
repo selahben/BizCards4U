508c17f10b859a2beb894c569bacf9aa6617ab76
Ben <selahben@hotmail.com>
2023-07-17 15:01:45 +0300

    mongodb connection for node.js 18+

--- biz-cards-server/app.js
+++ biz-cards-server/app.js
@@ -9,7 +9,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 mongoose
-  .connect("mongodb://localhost/my_rest_api")
+  .connect("mongodb://127.0.0.1:27017/my_rest_api")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB..."));
 
